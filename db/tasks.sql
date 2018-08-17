850d12f19b6b37e919185f46d5316e2c5cf24617
John Aynedjian <john.david.aynedjian@gmail.com>
2018-08-16 20:14:37 -0400

    db defs and seed data...

---
+++ db/tasks.sql
@@ -0,0 +1,7 @@
+
+insert into tasks (  description, assigned_to ) 
+values
+( 'Wash glassware', 1 ),
+( 'Arrange files', 2 ),
+( 'Run Cyanide analysis', 3 )
+;
---
+++ db/users.sql
@@ -0,0 +1,7 @@
+
+insert into users ( first_name, middle_name, last_name)
+values
+( 'Joe', 'S.', 'Kovacs'),
+( 'Jean', 'R.', 'Kovacs'),
+( 'John', 'D.', 'Aynedjian')
+;
